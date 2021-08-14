aa9b5d158c10a49f2854fed581dd2ee133ca9294
Niall Byrne <niall@niallbyrne.ca>
2021-08-14 12:38:16 -0400

    fix(COMPONENTS.BILLBOARD): properly center billboard

--- src/components/billboard/billboard.component.tsx
+++ src/components/billboard/billboard.component.tsx
@@ -9,7 +9,7 @@ const Billboard = ({ children, title }: BillboardProps) => {
   const bg = useColorModeValue("gray.200", "gray.800");
 
   return (
-    <Center height={"calc(100vh - 128px)"}>
+    <Center height={"calc(100vh + 32px)"}>
       <Box p={3} bg={bg} w={["90%", "80%", "70%"]}>
         <Flex justify={"center"}>
           <Text fontSize={["xl", "2xl", "3xl"]}>{title}</Text>
