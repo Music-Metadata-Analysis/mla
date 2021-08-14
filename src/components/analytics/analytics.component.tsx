c77b2e33bc578300238f5e7c606fd85ba3c91dd5
Niall Byrne <niall@niallbyrne.ca>
2021-08-14 12:39:19 -0400

    feat(COMPONENTS.ANALYTICS): create analytics clickthrough component

---
+++ src/components/analytics/analytics.component.tsx
@@ -0,0 +1,18 @@
+import useAnalytics from "../../hooks/analytics";
+
+interface AnalyticsWrapperProps {
+  buttonName: string;
+  children: React.ReactNode;
+}
+
+const AnalyticsWrapper = ({ buttonName, children }: AnalyticsWrapperProps) => {
+  const analytics = useAnalytics();
+
+  return (
+    <div onClick={(e) => analytics.trackButtonClick(e, buttonName)}>
+      {children}
+    </div>
+  );
+};
+
+export default AnalyticsWrapper;
