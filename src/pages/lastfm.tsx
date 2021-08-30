8490c1ec54dba6f66316d8420d6494fcfe4f0ad1
Niall Byrne <niall@niallbyrne.ca>
2021-08-30 12:06:11 -0400

    feat(PAGES.LASTFM): render 404 when no username is given

--- src/pages/lastfm.tsx
+++ src/pages/lastfm.tsx
@@ -1,18 +1,34 @@
-import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import FourOhFour from "./404";
+import ErrorBoundary from "../components/errors/boundary/error.boundary.component";
+//import Top20Report from "../components/reports/lastfm/top20/top20.component";
+import Events from "../config/events";
+import settings from "../config/lastfm";
+import routes from "../config/routes";
+import useLastFM from "../hooks/lastfm";
 import pagePropsGenerator from "../utils/page.props.static";
 
 export default function LastFM() {
-  const router = useRouter();
-  const { username } = router.query;
+  const [username, setUsername] = useState<string | null>(null);
+  const user = useLastFM();
+
+  useEffect(() => {
+    if (username === null) {
+      const urlParams = new URLSearchParams(window.location.search);
+      setUsername(urlParams.get(settings.search.fieldName));
+    }
+  }, [username]);
+
+  if (username === null) return <FourOhFour />;
 
   return (
-    <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <div>{`lastfm: username: ${username}`}</div>
-    </>
+    <ErrorBoundary
+      eventDefinition={Events.General.Error}
+      route={routes.home}
+      stateReset={user.clear}
+    >
+      <div>{username}</div>
+    </ErrorBoundary>
   );
 }
 
