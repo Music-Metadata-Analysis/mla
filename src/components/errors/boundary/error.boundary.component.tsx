d9723c47a35fcd5310dcb90f231d6f73f058aebf
Niall Byrne <niall@niallbyrne.ca>
2021-09-23 13:59:35 -0400

    fix(COMPONENTS.ERRORS): events trigger on err

--- src/components/errors/boundary/error.boundary.component.tsx
+++ src/components/errors/boundary/error.boundary.component.tsx
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
 import useAnalytics from "../../../hooks/analytics";
 import ErrorHandler, {
@@ -23,19 +22,17 @@ const ErrorBoundary = ({
   const analytics = useAnalytics();
   const router = useRouter();
 
-  useEffect(() => {
-    analytics.event(eventDefinition);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   const errorHandler = () => {
     stateReset();
     router.push(route);
   };
 
-  const fallbackRender = ({ error, resetErrorBoundary }: ErrorHandlerProps) => (
-    <ErrorHandler error={error} resetErrorBoundary={resetErrorBoundary} />
-  );
+  const fallbackRender = ({ error, resetErrorBoundary }: ErrorHandlerProps) => {
+    analytics.event(eventDefinition);
+    return (
+      <ErrorHandler error={error} resetErrorBoundary={resetErrorBoundary} />
+    );
+  };
 
   return (
     <ReactErrorBoundary fallbackRender={fallbackRender} onReset={errorHandler}>
