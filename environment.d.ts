16e2eb82811f3f78c915a320bd4fb2c7f4438439
Niall Byrne <niall@niallbyrne.ca>
2021-09-27 19:30:21 -0400

    feat(COMPONENTS.CONSENT): analytics cookie consent

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ import { MutableEnv } from "./environment.d";
 declare global {
   namespace NodeJS {
     export interface ProcessEnv extends MutableEnv {
+      NEXT_PUBLIC_ANALYTICS_UA_CODE: string;
       LAST_FM_KEY: string;
       INTEGRATION_TEST_LAST_FM_KEY: string;
     }
