f06a691b297d71daaf265267bbfeffbad71b8e2b
Niall Byrne <niall@niallbyrne.ca>
2021-10-01 09:44:40 -0400

    refactor(CONFIG.NAVBAR): export a single object

--- src/hooks/navbar.ts
+++ src/hooks/navbar.ts
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { settings } from "../config/navbar";
+import NavConfig from "../config/navbar";
 import { NavBarContext } from "../providers/navbar/navbar.provider";
 
 const useNavBar = () => {
   const { setIsVisible } = useContext(NavBarContext);
 
   const hideNavBar = () => {
-    if (window.innerHeight < settings.heightDuringInput) {
+    if (window.innerHeight < NavConfig.heightDuringInput) {
       setIsVisible(false);
     } else {
       setIsVisible(true);
