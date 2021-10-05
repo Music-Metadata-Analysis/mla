9578509a29373dd9116925f75a50917ca2239256
Niall Byrne <niall@niallbyrne.ca>
2021-10-05 12:04:11 -0400

    feat(COMPONENTS.NAVBAR): standardize home button

--- src/components/navbar/navbar.logo/tests/navbar.logo.component.test.tsx
+++ src/components/navbar/navbar.logo/tests/navbar.logo.component.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import translation from "../../../../../public/locales/en/navbar.json";
 import routes from "../../../../config/routes";
 import mockAnalyticsHook from "../../../../hooks/tests/analytics.mock";
+import mockRouter from "../../../../tests/fixtures/mock.router";
 import NavBarAvatar from "../../navbar.avatar/navbar.avatar.component";
 import NavBarLink from "../../navbar.link/navbar.link.component";
 import NavBarLogo from "../navbar.logo.component";
@@ -19,6 +20,11 @@ jest.mock("../../navbar.link/navbar.link.component", () => {
   return jest.fn().mockImplementation(() => <>{TitlePlaceHolder}</>);
 });
 
+jest.mock("next/router", () => ({
+  __esModule: true,
+  useRouter: () => mockRouter,
+}));
+
 const TitlePlaceHolder = "TitlePlaceHolder";
 
 describe("NavBarLogo", () => {
@@ -27,6 +33,7 @@ describe("NavBarLogo", () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
+    mockRouter.pathname = routes.home;
     arrange();
   });
 
@@ -41,6 +48,7 @@ describe("NavBarLogo", () => {
   it("should render the title SimpleLink with the correct props", () => {
     expect(NavBarLink).toBeCalledTimes(1);
     const call = (NavBarLink as jest.Mock).mock.calls[0][0];
+    expect(call.selected).toBe(routes.home === mockRouter.pathname);
     expect(call.href).toBe(routes.home);
     expect(call.children).toBe(translation.title);
     expect(call.trackButtonClick).toBe(mockAnalyticsHook.trackButtonClick);
