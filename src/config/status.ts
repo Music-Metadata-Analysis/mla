610ad98688500647149d31d4747d4310743c9386
Niall Byrne <niall@niallbyrne.ca>
2021-10-22 08:58:38 -0400

    feat(API): add jwt auth to lastfm endpoints

--- src/config/status.ts
+++ src/config/status.ts
@@ -6,6 +6,9 @@ export const STATUS_200_MESSAGE: StatusMessageType = {
 export const STATUS_400_MESSAGE: StatusMessageType = {
   detail: "Invalid Request.",
 };
+export const STATUS_401_MESSAGE: StatusMessageType = {
+  detail: "Unauthorized.",
+};
 export const STATUS_404_MESSAGE: StatusMessageType = {
   detail: "Resource not found.",
 };
