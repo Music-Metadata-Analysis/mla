fe4d38033990f40264a3f289ba19508b77b1db20
Niall Byrne <niall@niallbyrne.ca>
2021-10-24 17:24:04 -0400

    feat(AUTHENTICATION): disable google, add spotify

--- types.d/environment.d.ts
+++ types.d/environment.d.ts
@@ -6,18 +6,18 @@ declare namespace NodeJS {
     AUTH_FACEBOOK_SECRET: string;
     AUTH_GITHUB_ID: string;
     AUTH_GITHUB_SECRET: string;
-    AUTH_GOOGLE_ID: string;
-    AUTH_GOOGLE_SECRET: string;
     AUTH_MASTER_JWT_SECRET: string;
     AUTH_MASTER_JWT_SIGNING_KEY: string;
     AUTH_MASTER_SECRET_KEY: string;
+    AUTH_SPOTIFY_ID: string;
+    AUTH_SPOTIFY_SECRET: string;
     INTEGRATION_TEST_LAST_FM_KEY: string;
-    LASTFM_CACHE_AWS_S3_BUCKET_NAME: string;
     LASTFM_CACHE_AWS_CLOUDFRONT_DOMAIN_NAME: string;
+    LASTFM_CACHE_AWS_S3_BUCKET_NAME: string;
     LAST_FM_KEY: string;
     MLA_AWS_ACCESS_KEY_ID: string;
-    MLA_AWS_SECRET_ACCESS_KEY: string;
     MLA_AWS_REGION: string;
+    MLA_AWS_SECRET_ACCESS_KEY: string;
     NEXT_PUBLIC_ANALYTICS_UA_CODE: string;
   }
 }
