f260d7b30bd37a1e069e8b7e842a7b3429468470
Niall Byrne <niall@niallbyrne.ca>
2021-10-26 14:44:08 -0400

    feat(CLIENTS.API): add top tracks client

---
+++ src/clients/api/reports/lastfm/tests/top20.tracks.class.test.tsx
@@ -0,0 +1,44 @@
+import apiRoutes from "../../../../../config/apiRoutes";
+import LastFMReport from "../top20.tracks.class";
+
+jest.mock("../../../api.client.class", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      post: mockPost,
+    };
+  });
+});
+
+const mockPost = jest.fn();
+
+describe("LastFMTopTracksReport", () => {
+  const mockDispatch = jest.fn();
+  const mockEvent = jest.fn();
+  let instance: LastFMReport;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const arrange = () => {
+    return new LastFMReport(mockDispatch, mockEvent);
+  };
+
+  describe("when a request returns not found", () => {
+    beforeEach(() => {
+      instance = arrange();
+    });
+
+    it("should have the correct api route configured", () => {
+      expect(instance.route).toBe(apiRoutes.v1.reports.lastfm.top20tracks);
+    });
+
+    it("should have the correct integration configured", () => {
+      expect(instance.integration).toBe("LAST.FM");
+    });
+
+    it("should have the correct event type configured", () => {
+      expect(instance.eventType).toBe("TOP20 TRACKS");
+    });
+  });
+});
