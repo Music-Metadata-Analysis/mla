2bec86a7c8b1db82254f2369e23ac6ed55376548
Niall Byrne <niall@niallbyrne.ca>
2021-10-27 14:38:47 -0400

    feat(COMPONENTS.ANALYTICS): internal link tracking

---
+++ src/components/analytics/analytics.link.internal/analytics.link.internal.component.tsx
@@ -0,0 +1,21 @@
+import useAnalytics from "../../../hooks/analytics";
+
+interface AnalyticsLinkWrapperProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const AnalyticsLinkWrapper = ({
+  href,
+  children,
+}: AnalyticsLinkWrapperProps) => {
+  const analytics = useAnalytics();
+
+  return (
+    <div onClick={(e) => analytics.trackInternalLinkClick(e, href)}>
+      {children}
+    </div>
+  );
+};
+
+export default AnalyticsLinkWrapper;
