fce70edefc4283d8ec125567ea98626b4d0844ec
Niall Byrne <niall@niallbyrne.ca>
2021-10-28 15:39:10 -0400

    feat(COMPONENTS.POPUPS): time sensitive feedback

---
+++ src/config/popups.ts
@@ -0,0 +1,7 @@
+const settings = {
+  FeedBack: {
+    searchMetricCount: [5, 10, 25, 50],
+  },
+};
+
+export default settings;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import Consent from "../components/consent/consent.component";
 import NavBar from "../components/navbar/navbar.component";
+import PopUps from "../components/popups/root.popup";
 import NavConfig from "../config/navbar";
 import RootProvider from "../providers/root.provider";
 import type { AppProps } from "next/app";
@@ -14,6 +15,7 @@ function App({
     <RootProvider session={session} headerProps={headerProps}>
       <NavBar menuConfig={NavConfig.menuConfig} />
       <Component {...otherProps} />
+      <PopUps />
       <Consent />
     </RootProvider>
   );
