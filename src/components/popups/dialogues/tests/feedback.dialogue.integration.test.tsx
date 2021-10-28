49be2b8d2d22086c74d78d8714f513db167857f0
Niall Byrne <niall@niallbyrne.ca>
2021-10-28 15:39:10 -0400

    feat(COMPONENTS.POPUPS): time sensitive feedback

---
+++ src/components/popups/dialogues/tests/feedback.dialogue.integration.test.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import externalLinks from "../../../../config/external";
+import FeedbackDialogue, { testIDs } from "../feedback.dialogue";
+
+describe("FeedbackDialogue", () => {
+  const mockClose = jest.fn();
+  const mockMessage = "mockMessage";
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    arrange();
+  });
+
+  const arrange = () => {
+    render(<FeedbackDialogue message={mockMessage} onClose={mockClose} />);
+  };
+
+  describe("when rendered", () => {
+    it("should show the expected text", async () => {
+      expect(await screen.findByText(mockMessage)).toBeTruthy();
+    });
+  });
+
+  describe("when the icon is clicked", () => {
+    let link: HTMLLinkElement;
+
+    beforeEach(async () => {
+      link = (await screen.findByTestId(testIDs.FeedBackDialogueIcon))
+        ?.parentElement?.parentElement?.parentElement as HTMLLinkElement;
+    });
+
+    it("should navigate to the expected page", async () => {
+      expect(link).toHaveAttribute("href", externalLinks.svsContact);
+    });
+  });
+
+  describe("when the close button is clicked", () => {
+    beforeEach(async () => {
+      const button = await screen.findByTestId(
+        testIDs.FeedBackDialogueCloseButton
+      );
+      fireEvent.click(button);
+    });
+
+    it("should nolonger show the expected text", async () => {
+      expect(mockClose).toBeCalledTimes(1);
+    });
+  });
+});
