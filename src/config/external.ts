3d225360939fb7f6d1a6f5161faebee09e44dc13
Niall Byrne <niall@niallbyrne.ca>
2021-10-29 22:22:02 -0400

    feat(CONFIG): update legal links for production

--- src/config/external.ts
+++ src/config/external.ts
@@ -2,9 +2,9 @@ const links = {
   svs: "https://sharedvisionsolutions.com",
   svsContact: "https://sharedvisionsolutions.com/#contact",
   privacyPolicy:
-    "https://www.privacypolicygenerator.info/live.php?token=zT8UukSUp3qk1DIlL5gSZMe2268J0rzC",
+    "https://www.privacypolicygenerator.info/live.php?token=KleNb61tPe92EnRvz8Af8HrN5GSVmzlt",
   termsOfService:
-    "https://www.termsofservicegenerator.net/live.php?token=FAcWAi3Aa6hukBr4qAxekHn4zAr2ta5t",
+    "https://www.termsofservicegenerator.net/live.php?token=415MkfQ6JUa6CUX631kdrbtUNEH9VsL3",
 };
 
 export default links;
