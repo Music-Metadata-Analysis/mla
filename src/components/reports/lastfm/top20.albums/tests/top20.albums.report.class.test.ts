579b8e28c66cc6a14b159b7306e8c1547c3c8701
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-04-15 15:47:18 -0400

    test(REPORTS.LASTFM): spelling correction

--- src/components/reports/lastfm/top20.albums/tests/top20.albums.report.class.test.ts
+++ src/components/reports/lastfm/top20.albums/tests/top20.albums.report.class.test.ts
@@ -37,7 +37,7 @@ describe("Top20AlbumsReport", () => {
     report = new Report();
   });
 
-  it("should have the correct instaces values", () => {
+  it("should have the correct instance values", () => {
     expect(report.analyticsReportType).toBe("TOP20 ALBUMS");
     expect(report.drawerArtWorkAltText).toBe(
       "top20Albums.drawer.artWorkAltText"
