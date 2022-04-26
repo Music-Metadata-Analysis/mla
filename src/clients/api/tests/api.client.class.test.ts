3f789f19e03c19f7eaf01be1b9aaf8f158345429
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-04-26 08:47:32 -0400

    refactor(CLIENTS.API): enable multiple methods

--- src/clients/api/tests/api.client.class.test.ts
+++ src/clients/api/tests/api.client.class.test.ts
@@ -6,11 +6,12 @@ describe("APIClient", () => {
   const remotesite = "https://remotesite.com/";
   type responseType = { success: boolean };
   const mockFetchParams = {
-    mode: "cors",
+    body: null,
     credentials: "same-origin",
     headers: {
       "Content-Type": "application/json",
     },
+    mode: "cors",
     referrerPolicy: "same-origin",
   };
 
@@ -55,7 +56,7 @@ describe("APIClient", () => {
     };
 
     const arrange = () => {
-      return client.get<responseType>(remotesite);
+      return client.request<responseType>(remotesite);
     };
 
     describe("when an 'ok' status is returned", () => {
@@ -166,7 +167,7 @@ describe("APIClient", () => {
 
       it("should throw the correct error message", async () => {
         const test = async () => await arrange();
-        expect(test).rejects.toThrow(`GET: ${remotesite}`);
+        expect(test).rejects.toThrow(`${getParams.method}: ${remotesite}`);
       });
     });
 
@@ -181,7 +182,152 @@ describe("APIClient", () => {
 
       it("should throw the correct error message", async () => {
         const test = async () => arrange();
-        expect(test).rejects.toThrow(`GET: ${remotesite}`);
+        expect(test).rejects.toThrow(`${getParams.method}: ${remotesite}`);
+      });
+    });
+  });
+
+  describe("post", () => {
+    const postParams = {
+      ...mockFetchParams,
+      method: "POST",
+      cache: "no-cache",
+      body: JSON.stringify({ test: "post body" }),
+    };
+
+    const arrange = () => {
+      return client.request<responseType>(
+        remotesite,
+        "POST",
+        "no-cache",
+        JSON.parse(postParams.body)
+      );
+    };
+
+    describe("when an 'ok' status is returned", () => {
+      beforeEach(() => setupFetch({ success: true, status: 200 }));
+
+      it("should call the underlying fetch function correctly", async () => {
+        await arrange();
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should return a success message", async () => {
+        const response = await arrange();
+        expect(response).toStrictEqual({
+          status: 200,
+          headers: {},
+          response: { success: true },
+        });
+      });
+    });
+
+    describe("when a '401' status code is returned", () => {
+      beforeEach(() => setupFetch({ success: false, status: 401 }));
+
+      it("should call the underlying fetch function correctly", () => {
+        arrange();
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should return the correct error message", async () => {
+        const response = await arrange();
+        expect(response).toStrictEqual({
+          status: 401,
+          headers: {},
+          response: status.STATUS_401_MESSAGE,
+        });
+      });
+    });
+
+    describe("when a '404' status code is returned", () => {
+      beforeEach(() => setupFetch({ success: false, status: 404 }));
+
+      it("should call the underlying fetch function correctly", () => {
+        arrange();
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should return the correct error message", async () => {
+        const response = await arrange();
+        expect(response).toStrictEqual({
+          status: 404,
+          headers: {},
+          response: status.STATUS_404_MESSAGE,
+        });
+      });
+    });
+
+    describe("when a '429' status code is returned", () => {
+      beforeEach(() => setupFetch({ success: false, status: 429 }));
+
+      it("should call the underlying fetch function correctly", () => {
+        arrange();
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should return the correct error message", async () => {
+        const response = await arrange();
+        expect(response).toStrictEqual({
+          status: 429,
+          headers: {},
+          response: status.STATUS_429_MESSAGE,
+        });
+      });
+    });
+
+    describe("when a '503' status code is returned", () => {
+      beforeEach(() =>
+        setupFetch({ success: false, status: 503 }, [["retry-after", "0"]])
+      );
+
+      it("should call the underlying fetch function correctly", () => {
+        arrange();
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should return the correct error message", async () => {
+        const response = await arrange();
+        expect(response).toStrictEqual({
+          status: 503,
+          headers: { "retry-after": "0" },
+          response: status.STATUS_503_MESSAGE,
+        });
+      });
+    });
+
+    describe("when a 'not ok' status is returned", () => {
+      beforeEach(() => setupFetch({ success: false, status: 400 }));
+
+      it("should call the underlying fetch function correctly", () => {
+        arrange().catch(() => null);
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should throw the correct error message", async () => {
+        const test = async () => await arrange();
+        expect(test).rejects.toThrow(`${postParams.method}: ${remotesite}`);
+      });
+    });
+
+    describe("when a network error occurs", () => {
+      beforeEach(() => setupFetchWithNetworkError());
+
+      it("should call the underlying fetch function correctly", () => {
+        arrange().catch(() => null);
+        expect(fetch).toBeCalledTimes(1);
+        expect(fetch).toBeCalledWith(remotesite, postParams);
+      });
+
+      it("should throw the correct error message", async () => {
+        const test = async () => arrange();
+        expect(test).rejects.toThrow(`${postParams.method}: ${remotesite}`);
       });
     });
   });
