2c8faf33616372c2b498ed73b31fa6b2ab6e1280
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-04-26 14:51:35 -0400

    feat(PROVIDERS.USER): add sunburst encapsulation

---
+++ src/providers/user/encapsulations/lastfm/sunburst/user.state.base.sunburst.report.class.ts
@@ -0,0 +1,43 @@
+import UserBaseState from "../user.state.base.class";
+import type { LastFMClientParamsInterface } from "../../../../../types/clients/api/reports/lastfm.client.types";
+import type { AggregateBaseReportResponseInterface } from "../../../../../types/integrations/base.types";
+import type { LastFMUserStateBase } from "../../../../../types/user/state.types";
+
+abstract class UserSunBurstReportBaseState<ReportType> extends UserBaseState {
+  lastfmPrefix = "https://last.fm/music";
+  abstract errorMessage: string;
+
+  constructor(userProperties: LastFMUserStateBase) {
+    super(userProperties);
+  }
+
+  abstract getReport(): AggregateBaseReportResponseInterface<ReportType>;
+
+  abstract updateWithResponse(
+    response: unknown,
+    params: LastFMClientParamsInterface,
+    url: string
+  ): void;
+
+  abstract getNextStep(params: LastFMClientParamsInterface): void;
+
+  abstract removeEntity(params: LastFMClientParamsInterface): void;
+
+  throwError() {
+    throw new Error(this.errorMessage);
+  }
+
+  getReportContent() {
+    return this.getReport().content;
+  }
+
+  getReportStatus() {
+    return this.getReport().status;
+  }
+
+  getDispatchState() {
+    return this.userProperties.data.report;
+  }
+}
+
+export default UserSunBurstReportBaseState;
--- src/types/integrations/base.types.ts
+++ src/types/integrations/base.types.ts
@@ -1,7 +1,39 @@
+import type {
+  PlayCountByArtistReportInterface,
+  LastFMClientParamsInterface,
+} from "../clients/api/reports/lastfm.client.types";
+
 export interface BaseReportResponseInterface {
   albums?: unknown[];
   artists?: unknown[];
+  playCountByArtist?: AggregateBaseReportResponseInterface<
+    PlayCountByArtistReportInterface[]
+  >;
   tracks?: unknown[];
   image: unknown[];
   playcount: number;
 }
+
+export interface AggregateBaseReportResponseInterface<ReportContentType> {
+  status: {
+    complete: boolean;
+    steps_total: number;
+    steps_complete: number;
+    operation?: AggregateReportOperationType;
+  };
+  created: string;
+  content: ReportContentType;
+}
+
+export type AggregateReportOperationType = {
+  resource: string;
+  type:
+    | "Album Details"
+    | "Artist's Albums"
+    | "Top Artists"
+    | "Top Albums"
+    | "Track Details"
+    | "User Profile";
+  url: string;
+  params: LastFMClientParamsInterface;
+};
