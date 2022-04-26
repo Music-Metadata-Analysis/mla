e44b26bfe47b9906e8a33867cb491561c0ecc70f
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-04-26 08:23:40 -0400

    refacator(PROVIDERS.USER): reorg encapsulations

--- src/components/reports/lastfm/top20.tracks/top20.tracks.report.class.ts
+++ src/components/reports/lastfm/top20.tracks/top20.tracks.report.class.ts
@@ -1,5 +1,5 @@
 import routes from "../../../../config/routes";
-import UserTrackDataState from "../../../../providers/user/encapsulations/lastfm/user.state.track.class";
+import UserTrackDataState from "../../../../providers/user/encapsulations/lastfm/flipcard/user.state.track.flipcard.report.class";
 import FlipCardDrawer from "../common/flip.card.report.drawer/flip.card.report.drawer.component";
 import FlipCardBaseReport from "../common/flip.card.report/flip.card.report.base.class";
 
