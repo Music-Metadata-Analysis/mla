069ba0eeb7c9bd734c93800782a1a5e7bb4367d8
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-04-29 17:59:35 -0400

    feat(HOOKS.LASTFM) add playcount by artist

--- src/hooks/lastfm.ts
+++ src/hooks/lastfm.ts
@@ -1,26 +1,47 @@
 import React from "react";
 import useAnalytics from "./analytics";
+import LastFMPlayCountByArtistDataClient from "../clients/api/lastfm/data/sunburst/playcount.by.artist.sunburst.client.class";
 import LastFMTopAlbumsReport from "../clients/api/lastfm/reports/top20.albums.class";
 import LastFMTopArtistsReport from "../clients/api/lastfm/reports/top20.artists.class";
 import LastFMTopTracksReport from "../clients/api/lastfm/reports/top20.tracks.class";
+import PlayCountByArtistStateEncapsulation from "../providers/user/encapsulations/lastfm/sunburst/playcount.by.artist/user.state.playcount.by.artist.sunburst.report.class";
 import { UserContext } from "../providers/user/user.provider";
+import type { PlayCountByArtistReportInterface } from "../types/clients/api/lastfm/response.types";
+import type {
+  SunBurstReportConstructor,
+  SunBurstEncapsulationConstructor,
+} from "../types/clients/api/lastfm/sunburst.types";
+import type { Top20ReportConstructor } from "../types/clients/api/lastfm/top20.types";
+import type { userDispatchType } from "../types/user/context.types";
 import type { LastFMUserStateBase } from "../types/user/state.types";
 
 const useLastFM = () => {
   const analytics = useAnalytics();
-  const { userProperties, dispatch } = React.useContext(UserContext);
-  const top20AlbumsReport = new LastFMTopAlbumsReport(
-    dispatch,
-    analytics.event
-  );
-  const top20ArtistsReport = new LastFMTopArtistsReport(
-    dispatch,
-    analytics.event
-  );
-  const top20TracksReport = new LastFMTopTracksReport(
-    dispatch,
-    analytics.event
-  );
+  const { userProperties, dispatch } = React.useContext(UserContext) as {
+    userProperties: LastFMUserStateBase;
+    dispatch: userDispatchType;
+  };
+
+  const createTop20Report = (
+    reportClass: Top20ReportConstructor,
+    userName: string
+  ) => {
+    const instance = new reportClass(dispatch, analytics.event);
+    instance.retrieveReport({ userName });
+  };
+
+  const createSunburstReport = <AggregateReportType>(
+    reportClass: SunBurstReportConstructor<AggregateReportType>,
+    encapsulationClass: SunBurstEncapsulationConstructor<AggregateReportType>,
+    userName: string
+  ) => {
+    const instance = new reportClass(
+      dispatch,
+      analytics.event,
+      new encapsulationClass(userProperties)
+    );
+    instance.retrieveReport({ userName });
+  };
 
   const clear = (): void => {
     dispatch({ type: "ResetState" });
@@ -36,24 +57,33 @@ const useLastFM = () => {
   };
 
   const top20albums = (userName: string): void => {
-    top20AlbumsReport.retrieveReport({ userName });
+    createTop20Report(LastFMTopAlbumsReport, userName);
   };
 
   const top20artists = (userName: string): void => {
-    top20ArtistsReport.retrieveReport({ userName });
+    createTop20Report(LastFMTopArtistsReport, userName);
   };
 
   const top20tracks = (userName: string): void => {
-    top20TracksReport.retrieveReport({ userName });
+    createTop20Report(LastFMTopTracksReport, userName);
+  };
+
+  const playCountByArtist = (userName: string): void => {
+    createSunburstReport<PlayCountByArtistReportInterface[]>(
+      LastFMPlayCountByArtistDataClient,
+      PlayCountByArtistStateEncapsulation,
+      userName
+    );
   };
 
   return {
-    userProperties: userProperties as LastFMUserStateBase,
+    userProperties,
     clear,
     ready,
     top20albums,
     top20artists,
     top20tracks,
+    playCountByArtist,
   };
 };
 
--- src/types/clients/api/lastfm/sunburst.types.ts
+++ src/types/clients/api/lastfm/sunburst.types.ts
@@ -1,7 +1,9 @@
 import type SunburstDataPointClient from "../../../../clients/api/lastfm/data/sunburst/datapoints/sunburst.datapoint.client.base.class";
+import type LastFMSunburstDataClient from "../../../../clients/api/lastfm/data/sunburst/sunburst.client.base.class";
 import type UserSunBurstReportBaseState from "../../../../providers/user/encapsulations/lastfm/sunburst/user.state.base.sunburst.report.class";
 import type { EventCreatorType } from "../../../analytics.types";
 import type { userDispatchType } from "../../../user/context.types";
+import type { LastFMUserStateBase } from "../../../user/state.types";
 
 export interface StepInterface {
   getStep(): void;
@@ -18,3 +20,13 @@ export type SunBurstDataPointClientConstructor<EncapsulationType> = {
     encapsulatedState: UserSunBurstReportBaseState<EncapsulationType>
   ): SunburstDataPointClient<EncapsulationType, unknown>;
 };
+
+export type SunBurstReportConstructor<AggregateReportType> = new (
+  dispatch: userDispatchType,
+  eventCreator: EventCreatorType,
+  encapsulatedState: UserSunBurstReportBaseState<AggregateReportType>
+) => LastFMSunburstDataClient<AggregateReportType>;
+
+export type SunBurstEncapsulationConstructor<AggregateReportType> = new (
+  state: LastFMUserStateBase
+) => UserSunBurstReportBaseState<AggregateReportType>;
