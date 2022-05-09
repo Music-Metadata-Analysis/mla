a8da7ba4c7d0eeb67887baebbc9a03aa1b2b553d
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-05-09 15:23:54 -0400

    feat(PROVIDERS.USER): new actions, class hierarchy

---
+++ src/providers/user/states/datapoints/datapoint.success.class.ts
@@ -0,0 +1,23 @@
+import ReducerStateBaseClass from "../user.reducer.states.base.class";
+import type { UserStateInterface } from "../../../../types/user/state.types";
+
+class ReducerDataPointSuccessFetch extends ReducerStateBaseClass<"DataPointSuccessFetch"> {
+  type = "DataPointSuccessFetch" as const;
+
+  generateState(): UserStateInterface {
+    return {
+      data: {
+        integration: this.state.data.integration,
+        report: this.action.data,
+      },
+      error: null,
+      inProgress: false,
+      profileUrl: this.state.profileUrl,
+      ready: false,
+      retries: this.initialRetries,
+      userName: this.state.userName,
+    };
+  }
+}
+
+export default ReducerDataPointSuccessFetch;
