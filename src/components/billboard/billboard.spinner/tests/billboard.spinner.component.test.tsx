7014e4c558652ce1ddbb62a53ea4915e304d976f
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-09-10 14:19:57 -0400

    feat(BILLBOARD): improve responsiveness

--- src/components/billboard/billboard.spinner/tests/billboard.spinner.component.test.tsx
+++ src/components/billboard/billboard.spinner/tests/billboard.spinner.component.test.tsx
@@ -1,5 +1,6 @@
-import { Spinner } from "@chakra-ui/react";
+import { Flex, Spinner } from "@chakra-ui/react";
 import { render, screen } from "@testing-library/react";
+import { settings } from "../../../../config/billboard";
 import mockColourHook from "../../../../hooks/tests/colour.hook.mock";
 import checkMockCall from "../../../../tests/fixtures/mock.component.call";
 import BillBoard from "../../billboard.component";
@@ -17,7 +18,7 @@ jest.mock("@chakra-ui/react", () => {
   const {
     factoryInstance,
   } = require("../../../../tests/fixtures/mock.chakra.react.factory.class");
-  return factoryInstance.create(["Spinner"]);
+  return factoryInstance.create(["Flex", "Spinner"]);
 });
 
 const createMockedComponent = (name: string) => {
@@ -40,52 +41,123 @@ describe("BillBoardSpinner", () => {
     },
     thickness: "8px",
   };
+  const originalWindowHeight = window.innerHeight;
+  let visible: boolean;
 
-  const arrange = (visibility: boolean) => {
+  afterAll(() => (window.innerHeight = originalWindowHeight));
+
+  const actRender = (visibility: boolean) => {
     render(<BillBoardSpinner visible={visibility} title={testTitle} />);
   };
 
   beforeEach(() => jest.clearAllMocks());
 
-  describe("visible is true", () => {
-    beforeEach(() => arrange(true));
-
-    it("should render the billboard", () => {
+  const checkBillBoardComponent = () => {
+    it("should call the BillBoard component with the correct props", () => {
       expect(BillBoard).toBeCalledTimes(1);
       checkMockCall(BillBoard, { title: testTitle });
     });
+  };
 
-    it("should render the spinner", async () => {
+  const checkFlexComponent = ({
+    expectedPaddingBottom,
+  }: {
+    expectedPaddingBottom: number;
+  }) => {
+    it("should call the Flex component with the correct props", () => {
+      expect(Flex).toBeCalledTimes(1);
+      checkMockCall(Flex, {
+        justify: "center",
+        pb: expectedPaddingBottom,
+        pt: 10,
+      });
+    });
+  };
+
+  const checkSpinnerComponent = () => {
+    it("should render the Spinner component with the correct props", () => {
       expect(Spinner).toBeCalledTimes(1);
       checkMockCall(Spinner, spinnerProps);
     });
+  };
 
-    it("should render the spinner as visible", async () => {
-      const spinner = await screen.findByTestId(
-        testIDs.BillboardSpinnerVisibilityControl
-      );
-      expect(spinner).toBeVisible();
+  describe("visible is true", () => {
+    beforeEach(() => (visible = true));
+
+    describe("when the screen height is < the threshold", () => {
+      beforeEach(() => {
+        window.innerHeight = settings.minimumTitleHeight - 1;
+        actRender(visible);
+      });
+
+      checkBillBoardComponent();
+      checkFlexComponent({ expectedPaddingBottom: 10 });
+      checkSpinnerComponent();
+
+      it("should render the Spinner as visible", async () => {
+        const spinner = await screen.findByTestId(
+          testIDs.BillboardSpinnerVisibilityControl
+        );
+        expect(spinner).toBeVisible();
+      });
+    });
+
+    describe("when the screen height is >= the threshold", () => {
+      beforeEach(() => {
+        window.innerHeight = settings.minimumTitleHeight;
+        actRender(visible);
+      });
+
+      checkBillBoardComponent();
+      checkFlexComponent({ expectedPaddingBottom: 20 });
+      checkSpinnerComponent();
+
+      it("should render the Spinner as visible", async () => {
+        const spinner = await screen.findByTestId(
+          testIDs.BillboardSpinnerVisibilityControl
+        );
+        expect(spinner).toBeVisible();
+      });
     });
   });
 
   describe("visible is false", () => {
-    beforeEach(() => arrange(false));
+    beforeEach(() => (visible = false));
 
-    it("should render the billboard", () => {
-      expect(BillBoard).toBeCalledTimes(1);
-      checkMockCall(BillBoard, { title: testTitle });
+    describe("when the screen height is < the threshold", () => {
+      beforeEach(() => {
+        window.innerHeight = settings.minimumTitleHeight - 1;
+        actRender(visible);
+      });
+
+      checkBillBoardComponent();
+      checkFlexComponent({ expectedPaddingBottom: 10 });
+      checkSpinnerComponent();
+
+      it("should render the Spinner as NOT visible", async () => {
+        const spinner = await screen.findByTestId(
+          testIDs.BillboardSpinnerVisibilityControl
+        );
+        expect(spinner).not.toBeVisible();
+      });
     });
 
-    it("should render the spinner", async () => {
-      expect(Spinner).toBeCalledTimes(1);
-      checkMockCall(Spinner, spinnerProps);
-    });
+    describe("when the screen height is >= the threshold", () => {
+      beforeEach(() => {
+        window.innerHeight = settings.minimumTitleHeight;
+        actRender(visible);
+      });
 
-    it("should render the spinner as NOT visible", async () => {
-      const spinner = await screen.findByTestId(
-        testIDs.BillboardSpinnerVisibilityControl
-      );
-      expect(spinner).not.toBeVisible();
+      checkBillBoardComponent();
+      checkFlexComponent({ expectedPaddingBottom: 20 });
+      checkSpinnerComponent();
+
+      it("should render the Spinner as NOT visible", async () => {
+        const spinner = await screen.findByTestId(
+          testIDs.BillboardSpinnerVisibilityControl
+        );
+        expect(spinner).not.toBeVisible();
+      });
     });
   });
 });
